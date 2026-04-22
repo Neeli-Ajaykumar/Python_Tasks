cc6a35ce5950db253e424cadb3e29745a989dc48
Neeli-AjayKumar <ajayneeli15@gmail.com>
2026-04-22 10:04:07 +0530

    initial commit

--- AnalyticsData-1/Sample-Project/sample.py
+++ AnalyticsData-1/Sample-Project/sample.py
@@ -1,19 +1,26 @@
+#import Required Libraries
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# Load Dataset
 d = pd.read_csv("railway_gauges.csv")
 
+# Display first 5 rows and last 5 rows
 print(d.head())
 print(d.tail())
 
+# Droping Total colum from the Dataset
 d = d.drop('Total', axis=1)
 
+# plotting bar graph
 ax = d.plot(x="Year", kind="bar")
 
+# Adding labels and title to the bar graph
 plt.xticks(rotation=70)
 plt.xlabel("Year")
 plt.ylabel("Total")
 plt.title("Gauges: Number of railway tracks installed per year")
 
+# saving figure and dislaying
 plt.savefig("rail_gauges.png")
 plt.show()
